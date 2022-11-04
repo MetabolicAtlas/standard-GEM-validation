4dcc451cb9ca0075e370bb9fef710808abc696d1
famosab <famke.baeuerle@gmail.com>
2022-11-04 11:11:46 +0100

    feat: restructure memote as valid return

--- tests/memote.py
+++ tests/memote.py
@@ -6,11 +6,12 @@ from memote.support import consistency
 from memote.support import consistency_helpers as con_helpers
 
 def get_consistency(model_filename):
+    errors = ''
+    is_consistent = 'Not tested'
     try:
         model = cobra.io.read_sbml_model(model_filename + '.xml')
         is_consistent = consistency.check_stoichiometric_consistency(model)
     except Exception as e:
-        is_consistent = 'Not applicable'
         errors = json.dumps(str(e))
         print(e)
     
