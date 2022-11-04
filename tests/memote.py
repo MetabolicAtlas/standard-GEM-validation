a20ac1a87379f354e729df657dc83b51bf50ad06
famosab <famke.baeuerle@gmail.com>
2022-11-04 09:44:26 +0100

    feat: memote, make errors available

--- tests/memote.py
+++ tests/memote.py
@@ -8,10 +8,10 @@ from memote.support import consistency_helpers as con_helpers
 def get_consistency(model_filename):
     try:
         model = cobra.io.read_sbml_model(model_filename + '.xml')
+        is_consistent = consistency.check_stoichiometric_consistency(model)
     except Exception as e:
+        is_consistent = 'Not applicable'
         errors = json.dumps(str(e))
         print(e)
-        
-    is_consistent = consistency.check_stoichiometric_consistency(model)
     
     return {'memote-tests': { memote.__version__ : is_consistent, 'errors': errors } }
